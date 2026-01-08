b8a3e741dbab723313137d19bb26236b93d3bbe7
YehiaHany22 <yehiahany091@gmail.com>
2026-01-08 02:39:28 +0200

    feat(ui):create radio screen.

--- lib/home_screen/tabs/radio_tab/radio_tab.dart
+++ lib/home_screen/tabs/radio_tab/radio_tab.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:islami/core/utils/device_dimensions.dart';
+import 'package:islami/home_screen/tabs/radio_tab/widgets/radio_container.dart';
+import 'package:islami/home_screen/tabs/radio_tab/widgets/tab_bar_switcher_radio.dart';
+import 'package:islami/models/name_of_radio_perfomers.dart';
 
 class RadioTab extends StatelessWidget {
   const RadioTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(color: Colors.green);
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: context.width * 0.0465),
+      child: Column(
+        spacing: context.height * 0.018,
+        children: [
+          TabBarSwitcherRadio(),
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return RadioContainer(
+                  name: NameOfRadioPerformers().nameOfPerformers[index],
+                );
+              },
+              itemCount: NameOfRadioPerformers().nameOfPerformers.length,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/models/name_of_radio_perfomers.dart
@@ -0,0 +1,8 @@
+class NameOfRadioPerformers {
+  List<String> nameOfPerformers = [
+    "Radio Ibrahim Al-Akdar",
+    "Radio Al-Qaria Yassen",
+    "Radio Ahmed Al-trabulsi",
+    "Radio Addokali Mohammad Alalim",
+  ];
+}
